ed1f13f8fae04e4897379ef70ec8f8aae5333cd2
Dusan Krstic <admin@inter-coder.com>
2015-05-12 22:01:39 +0200

    second

--- crossdomain.js
+++ crossdomain.js
@@ -5,7 +5,7 @@ function xdomain(par){//crossdomain function
 	 par.url = 'http://query.yahooapis.com/v1/public/yql?q='+encodeURIComponent('select * from html WHERE url="'+par.url+'" AND xpath="'+par.xpath+'"')+'&format='+par.type+'&callback=cb';
 	 xmlhttp.open("GET",par.url,false);
 	 xmlhttp.onload=function(){
-	 	function cb(d){return d;}//callBack bunction for returning results from query
+	 	function cb(d){return d;}//callBack function for returning results from query
 		par.callBack(eval(xmlhttp.responseText));
 	 };
 	 xmlhttp.send();
